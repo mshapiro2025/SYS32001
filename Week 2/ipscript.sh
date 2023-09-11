c2c7c9bfc4e424e095c9463217d1196c043b06e3
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-09-11 18:45:08 -0400

    Update ipscript.sh- adding more comments

--- Week 2/ipscript.sh
+++ Week 2/ipscript.sh
@@ -4,4 +4,11 @@
 
 # Storyline: this script uses the ip addr command to only show the relevant IP address
 
+# The first part of this script is the ip addr command
+# The second part of this script looks for the string 'inet', which will start all IP addresses with ip addr
+# The third part of this script excludes the address 127.0.0.1, which is the local loopback address and is not relevant
+# The final part of this script is using awk.
+# awk breaks down the text it is given by white space, so each string separated by white space gets assigned a variable
+# starting at $1. Since 'inet' is the first string and the IP address is the second, I used $2. 
+
 ip addr | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'
